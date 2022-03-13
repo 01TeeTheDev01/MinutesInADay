750c087d1bcc9878efd67c924e052692cc924803
Teethedev@outlook.com <Teethedev@outlook.com>
2022-03-13 10:16:09 +0200

    Add project files.

---
+++ MinutesInADay/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinutesInADay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n===|= Minutes - calculator =|===\n");
+
+            Console.WriteLine(CalculateMinutes(day: 65, hours: 15, minutes: 6, seconds: 3));
+
+            Console.ReadLine();
+        }
+
+        private static string CalculateMinutes(int day, int hours, int minutes, int seconds)
+        {
+            if (day >= 0 && (hours > 0 && hours <= 24) && (minutes > 0 && minutes <= 60) && (seconds > 0 && seconds <= 60))
+            {
+                int totalMinutes = (seconds * minutes) * hours;
+
+                DateTime date = DateTime.Now.AddDays(day);
+
+                switch (date.Month)
+                {
+                    case (int)Months.Jan:
+                        return $"{date.ToLongDateString()} has a total of {totalMinutes} minutes according to supplied parameters.";
+
+                    case (int)Months.Feb:
+                        return $"{date.ToLongDateString()} has a total of {totalMinutes} minutes according to supplied parameters.";
+
+                    case (int)Months.Mar:
+                        return $"{date.ToLongDateString()} has a total of {totalMinutes} minutes according to supplied parameters.";
+
+                    case (int)Months.Apr:
+                        return $"{date.ToLongDateString()} has a total of {totalMinutes} minutes according to supplied parameters.";
+
+                    case (int)Months.May:
+                        return $"{date.ToLongDateString()} has a total of {totalMinutes} minutes according to supplied parameters.";
+
+                    case (int)Months.Jun:
+                        return $"{date.ToLongDateString()} has a total of {totalMinutes} minutes according to supplied parameters.";
+
+                    case (int)Months.Jul:
+                        return $"{date.ToLongDateString()} has a total of {totalMinutes} minutes according to supplied parameters.";
+
+                    case (int)Months.Aug:
+                        return $"{date.ToLongDateString()} has a total of {totalMinutes} minutes according to supplied parameters.";
+
+                    case (int)Months.Sep:
+                        return $"{date.ToLongDateString()} has a total of {totalMinutes} minutes according to supplied parameters.";
+
+                    case (int)Months.Oct:
+                        return $"{date.ToLongDateString()} has a total of {totalMinutes} minutes according to supplied parameters.";
+
+                    case (int)Months.Nove:
+                        return $"{date.ToLongDateString()} has a total of {totalMinutes} minutes according to supplied parameters.";
+
+                    case (int)Months.Dec:
+                        return $"{date.ToLongDateString()} has a total of {totalMinutes} minutes according to supplied parameters.";
+
+                    default:
+                        return "Month is invalid.";
+                }
+            }
+
+            return "Parameters are out of bounds.";
+        }
+
+        private enum Months
+        { 
+            Jan = 1,
+            Feb,
+            Mar,
+            Apr,
+            May,
+            Jun,
+            Jul,
+            Aug,
+            Sep,
+            Oct,
+            Nove,
+            Dec
+        }
+    }
+}
